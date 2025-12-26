eb1d2a73af6d3cd1f619f169b6869fbc42ad53ba
Abhishekdhal <abhishekdhalofficial@gmail.com>
2025-12-26 18:09:49 +0530

    fix(models): resolve mongoose pre-save hook error in User.js

--- models/User.js
+++ models/User.js
@@ -11,21 +11,32 @@ const userSchema = mongoose.Schema({
     phone: { type: String, required: true },
     role: { type: String, default: 'student' }, 
     
-    // --- NEW: OTP FIELDS ---
+    // OTP FIELDS
     resetPasswordOTP: { type: String },
     resetPasswordExpires: { type: Date },
     otpRequestCount: { type: Number, default: 0 },
     lastOtpRequest: { type: Date }
 }, { timestamps: true });
 
-// Hash password before saving
-userSchema.pre('save', async function (next) {
+/**
+ * UPDATED: Async Pre-save hook
+ * We removed 'next' because Mongoose handles async functions 
+ * by waiting for the returned promise to resolve.
+ */
+userSchema.pre('save', async function () {
+    // Only hash the password if it has been modified (or is new)
     if (!this.isModified('password')) { 
-        return next(); 
+        return; 
     }
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    } catch (error) {
+        // If using async, throwing an error will stop the save and 
+        // pass the error to the save() callback/promise.
+        throw new Error(error);
+    }
 });
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
@@ -34,6 +45,43 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
+
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcryptjs');
+
+// const userSchema = mongoose.Schema({
+//     name: { type: String, required: true },
+//     email: { type: String, required: true, unique: true },
+//     password: { type: String, required: true },
+//     school: { type: String, required: true },
+//     branch: { type: String, required: true },
+//     semester: { type: String, required: true },
+//     phone: { type: String, required: true },
+//     role: { type: String, default: 'student' }, 
+    
+//     // --- NEW: OTP FIELDS ---
+//     resetPasswordOTP: { type: String },
+//     resetPasswordExpires: { type: Date },
+//     otpRequestCount: { type: Number, default: 0 },
+//     lastOtpRequest: { type: Date }
+// }, { timestamps: true });
+
+// // Hash password before saving
+// userSchema.pre('save', async function (next) {
+//     if (!this.isModified('password')) { 
+//         return next(); 
+//     }
+//     const salt = await bcrypt.genSalt(10);
+//     this.password = await bcrypt.hash(this.password, salt);
+//     next();
+// });
+
+// userSchema.methods.matchPassword = async function (enteredPassword) {
+//     return await bcrypt.compare(enteredPassword, this.password);
+// };
+
+// const User = mongoose.model('User', userSchema);
+// module.exports = User;
 
 // const mongoose = require('mongoose');
 // const bcrypt = require('bcryptjs');
